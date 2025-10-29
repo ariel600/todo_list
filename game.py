ffcdbc1fd684b0df42e1b5a5409124b576729767
Ariel_Naiman <a0527633600@gmail.com>
2025-10-29 10:03:07 +0200

    adding show_all_tasks func

--- game.py
+++ game.py
@@ -1,6 +1,15 @@
-todo_list = []
+todo_list = ["בננה", "אבטיח"]
 
 def add_task(tasks: list, task: str) -> None:
     tasks.append(task)
     pass
 
+def show_all_tasks(tasks: list) -> None:
+    if len(tasks) == 0:
+        print("You don't have any tasks yet.")
+    else:
+        index = 0
+        for task in tasks:
+            index += 1
+            print(f"{index}. {task}")
+    pass
