233d72ca1b4ea3254c122dbdca7b46be5843cae9
Ariel_Naiman <a0527633600@gmail.com>
2025-10-29 10:22:50 +0200

    adding get_user_coise func

--- game.py
+++ game.py
@@ -13,3 +13,15 @@ def show_all_tasks(tasks: list) -> None:
             index += 1
             print(f"{index}. {task}")
     pass
+
+def get_user_choice() -> str:
+    choice = input(
+    """
+    1. הוספת משימה
+    2. הצגת המשימות
+    3. מחיקת משימה
+    4. עריכת משימה
+    5. יציאה
+    בחר את האפשרות הרצויה: """
+    )
+    return choice
