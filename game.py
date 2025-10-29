f82fc94e77c906321a7aa5c0e21b83f9f16bd534
Ariel_Naiman <a0527633600@gmail.com>
2025-10-29 10:28:35 +0200

    File modification

--- game.py
+++ game.py
@@ -11,13 +11,17 @@ def show_all_tasks(tasks: list) -> None:
         index = 0
         for task in tasks:
             index += 1
-            print(f"""{index}. task""")
+            print(f"{index}. {task}")
     pass
 
 def get_user_choice() -> str:
-    pass
-
-def main() -> None:
-    pass
-
-print(show_all_tasks(todo_list))
+    choice = input(
+    """
+    1. הוספת משימה
+    2. הצגת המשימות
+    3. מחיקת משימה
+    4. עריכת משימה
+    5. יציאה
+    בחר את האפשרות הרצויה: """
+    )
+    return choice
