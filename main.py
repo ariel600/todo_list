fe2a15f9224a691674e7bb77309ce995fae12f81
Ariel_Naiman <a0527633600@gmail.com>
2025-10-29 10:25:05 +0200

    adding get_user_coise func

---
+++ main.py
@@ -0,0 +1,27 @@
+todo_list = ["בננה", "אבטיח"]
+
+def add_task(tasks: list, task: str) -> None:
+    tasks.append(task)
+    pass
+
+def show_all_tasks(tasks: list) -> None:
+    if len(tasks) == 0:
+        print("You don't have any tasks yet.")
+    else:
+        index = 0
+        for task in tasks:
+            index += 1
+            print(f"{index}. {task}")
+    pass
+
+def get_user_choice() -> str:
+    choice = input(
+    """
+    1. הוספת משימה
+    2. הצגת המשימות
+    3. מחיקת משימה
+    4. עריכת משימה
+    5. יציאה
+    בחר את האפשרות הרצויה: """
+    )
+    return choice
